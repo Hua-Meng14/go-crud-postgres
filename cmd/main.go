1e65e14389d5a3c9b0750b607e686ecd45fc3ce5
Hua-Meng14 <lim.huameng19@kit.edu.kh>
2024-04-03 21:05:00 +0700

    feat: crud order, user, product

---
+++ cmd/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+
+	"github.com/Hua-Meng14/go-crud-postgres/database"
+	"github.com/Hua-Meng14/go-crud-postgres/routes"
+	"github.com/gofiber/fiber/v2"
+)
+
+func welcome(c *fiber.Ctx) error {
+	return c.SendString("Welcome to an Awesome API")
+}
+
+func setupRoutes(app *fiber.App) {
+	// Welcome endpoint
+	app.Get("/api", welcome)
+	// User endpoints
+	app.Post("/api/users", routes.CreateUser)
+	app.Get("/api/users", routes.GetUsers)
+	// app.Get("/api/users/:id", routes.GetUserById)
+	// app.Delete("/api/users/:id", routes.DeleteUser)
+	// Product endpoints
+	// app.Post("/api/products", routes.CreateProduct)
+	// app.Get("/api/products", routes.GetProducts)
+	// app.Get("/api/products/:id", routes.GetProduct)
+	// app.Put("/api/products/:id", routes.UpdateProduct)
+	// Order endpoints
+	// app.Post("/api/orders", routes.CreateOrder)
+	// app.Get("/api/orders", routes.GetOrders)
+	// app.Get("/api/orders/:id", routes.GetOrder)
+}
+
+func main() {
+	database.ConnectDb()
+
+	app := fiber.New()
+	setupRoutes(app)
+
+	log.Fatal(app.Listen(":3000"))
+
+}
